833ef1df945c8eb699f3b59d5b7223ada92b8f25
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-11-18 09:58:46 +0700

    demo Array List

--- src/demo2/Main.java
+++ src/demo2/Main.java
@@ -1,9 +1,32 @@
 package demo2;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        Bike b = new Bike();
-        b.setBrand("Honda");
-        b.setYear(-100);
+         int[] arr = new int[5];// int arr[5]
+
+        ArrayList ls = new ArrayList();
+        ls.add(1);
+        ls.add("2");
+
+        System.out.println(ls.get(0));
+        System.out.println(ls.get(1));
+
+        System.out.println(ls.size());
+
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(2);
+//        intList.add("3");
+
+        ArrayList<String> strList = new ArrayList<>();
+        strList.add("Hello");
+//        strList.add(2);
+
+        ArrayList<Bike> bList = new ArrayList<>();
+        Bike b1 = new Bike();
+        bList.add(b1);
+
+        bList.add(new Bike());
     }
 }
