e2584b766f23f2eb1c406bc970ed33b9543afc41
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-11-23 11:06:40 +0700

    lab 10

---
+++ src/lab/lab10/PhoneBook.java
@@ -0,0 +1,28 @@
+package lab.lab10;
+
+public class PhoneBook extends Phone{
+    @Override
+    public void insertPhone(String name, String phone) {
+
+    }
+
+    @Override
+    public void removePhone(String name) {
+
+    }
+
+    @Override
+    public void updatePhone(String name, String newphone) {
+
+    }
+
+    @Override
+    public PhoneNumber searchPhone(String name) {
+        return null;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
